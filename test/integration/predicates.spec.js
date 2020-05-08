ff05381ee00e41be6621b00bb983b1038fce91e5
Martijn van de Rijdt <martijn@enketo.org>
2020-05-08 14:39:38 -0600

    added comments to test

--- test/integration/predicates.spec.js
+++ test/integration/predicates.spec.js
@@ -1,4 +1,4 @@
-const { assertNumberValue, assertTrue, initDoc } = require('../helpers');
+const { assertNumberValue, assertStringValue, assertTrue, initDoc } = require('../helpers');
 
 describe('predicates with function calls', ()=> {
 
@@ -8,15 +8,18 @@ describe('predicates with function calls', ()=> {
           <item>
             <number>4</number>
           </item>
+          <b/>
+          <b/>
         </data>`);
 
         assertTrue('count(/data/item[true()]) = 1');
+        assertNumberValue('count(/data/b[round(2.44) = 2])', 2);
         assertNumberValue('/data/item[true()]/number', 4);
-        assertNumberValue('count(/data/item/number[round(2.44) = 2])', 1);
-        assertNumberValue('/data/item[true()]/number + 1', 5);
-        assertNumberValue('/data/item[string-length("a") = 1]/number + 2', 6);
-        assertNumberValue('/data/item[string-length(./number)=1]/number + 3', 7);
-    });
+        assertNumberValue('/data/item[true()]/number + 1', 5); // returns undefined, but probably not related to predicate
+        assertStringValue('/data/item[true()]/number + 1', '5'); // returns '41', but probably not related to predicate
+        assertNumberValue('/data/item[string-length("a") = 1]/number + 2', 6); // returns undefined, but probably not related to predicate
+        assertNumberValue('/data/item[string-length(./number)=1]/number + 3', 7); // returns undefined, but probably not related to predicate
+     });
 
     it('with extended functions', () => {
         initDoc(`
@@ -30,7 +33,7 @@ describe('predicates with function calls', ()=> {
         assertNumberValue('/data/item[tan(./number) > 1]/number', 4);
     });
     
-    // I put this one separate as it has a different 'too many args' error, and there may be have multiple causes for failure
+    // I put this one separate as it has a different 'too many args' error, and there may be multiple causes for failure
     it('with the #selected function', () => {
         const doc = initDoc(`
           <data>
@@ -39,6 +42,7 @@ describe('predicates with function calls', ()=> {
             <a>c</a>
           </data>`);
     
+        // assertTrue('selected("a b", "a")');
         assertNumberValue('count(/data/a[selected("a b", "a")])', 3);
       });
 });
