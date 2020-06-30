1ae2e9571e676b3c735382b1ef6d246f4f090abc
Alex Anderson <alxndrsn>
2020-06-30 22:24:38 +0000

    Remove TODO markers

--- src/utils/xpath-cast.js
+++ src/utils/xpath-cast.js
@@ -36,13 +36,13 @@ function asString(r) {
 }
 
 // Per https://www.w3.org/TR/1999/REC-xpath-19991116/#dt-string-value:
-// [DONE] 1. > The string-value of the root node is the concatenation of the string-values of all text node descendants of the root node in document order.
-// [DONE] 2. > The string-value of an element node is the concatenation of the string-values of all text node descendants of the element node in document order.
-// [DONE] 3. > An attribute node has a string-value. The string-value is the normalized value as specified by the XML Recommendation [XML]. An attribute whose normalized value is a zero-length string is not treated specially: it results in an attribute node whose string-value is a zero-length string.
-// [TODO] 4. > The string-value of a namespace node is the namespace URI that is being bound to the namespace prefix; if it is relative, it must be resolved just like a namespace URI in an expanded-name.
-// [DONE] 5. > The string-value of a processing instruction node is the part of the processing instruction following the target and any whitespace.
-// [DONE] 6. > The string-value of comment is the content of the comment not including the opening <!-- or the closing -->.
-// [DONE] 7. > The string-value of a text node is the character data.
+// 1. > The string-value of the root node is the concatenation of the string-values of all text node descendants of the root node in document order.
+// 2. > The string-value of an element node is the concatenation of the string-values of all text node descendants of the element node in document order.
+// 3. > An attribute node has a string-value. The string-value is the normalized value as specified by the XML Recommendation [XML]. An attribute whose normalized value is a zero-length string is not treated specially: it results in an attribute node whose string-value is a zero-length string.
+// 4. > The string-value of a namespace node is the namespace URI that is being bound to the namespace prefix; if it is relative, it must be resolved just like a namespace URI in an expanded-name.
+// 5. > The string-value of a processing instruction node is the part of the processing instruction following the target and any whitespace.
+// 6. > The string-value of comment is the content of the comment not including the opening <!-- or the closing -->.
+// 7. > The string-value of a text node is the character data.
 function nodeToString(node) {
   switch(node.nodeType) {
     case Node.DOCUMENT_NODE: return node.documentElement.textContent;
