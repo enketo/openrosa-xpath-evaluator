4369370d4a8b5790b04fe6a929e66dff870c42b0
Alex Anderson <alxndrsn>
2020-11-26 06:32:44 +0000

    modernise xpr

--- src/xpr.js
+++ src/xpr.js
@@ -2,8 +2,8 @@
  * Internal representations of XPathResults
  */
 module.exports = {
-  boolean: function(v) { return { t:'bool', v:v }; },
-  number:  function(v) { return { t:'num',  v:v }; },
-  string:  function(v) { return { t:'str',  v:v }; },
-  date:    function(v) { return { t:'date', v:v }; },
+  boolean: v => ({ t:'bool', v }),
+  number:  v => ({ t:'num',  v }),
+  string:  v => ({ t:'str',  v }),
+  date:    v => ({ t:'date', v }),
 };
