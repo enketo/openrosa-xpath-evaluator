f88ede98a8fc8ffa16275781efe0ee532181388b
Alex Anderson <alxndrsn>
2020-12-15 06:12:42 +0000

    Fix incorrect comment

--- test/integration/index.js
+++ test/integration/index.js
@@ -1,6 +1,6 @@
 // see: https://github.com/ryanclark/karma-webpack#alternative-usage
 
-// require all modules ending in "_test" from the
+// require all modules ending in ".spec" from the
 // current directory and all subdirectories
 const testsContext = require.context('.', true, /.spec$/);
 
