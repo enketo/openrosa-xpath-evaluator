36e4353234769cb172ce7a0226b7e9011df31cbb
Martijn van de Rijdt <martijn@enketo.org>
2021-01-07 11:06:48 -0700

    Add failing test for #71

--- test/integration/openrosa-xpath/if.spec.js
+++ test/integration/openrosa-xpath/if.spec.js
@@ -64,6 +64,42 @@ describe('#if()', () => {
       const node = doc.getElementById('FunctionChecklistCase0');
       assertStringValue(node, null, 'if(false(), "yes", concat(/div/div[@id="FunctionChecklistCaseNo"], "-no"))', 'no-no');
     });
+  });
+
+  describe('should deal with nesting and lengthy or/and clauses', ()=> {
+
+    const doc = initDoc(`
+    <data>
+      <a/>
+      <b/>
+      <c>1</c>
+      <d>0</d>
+    </data>`);
+
+     // TODO: this is a lazy test taken directly from a real form. It probably should be removed the minimal test cases below it seem to be sufficient, but it will be helpful during bug fixing. None of these nodes exist in the doc.
+     it(`long sequence of "and" clauses and nested if() with long sequence of "or clauses" (non-minimized test case)`, () => {
+      assertStringValue(doc, null, 'if( /model/instance[1]/data/page-welcome/GRP_ELIG/AGE_IC ="1" and /model/instance[1]/data/page-welcome/GRP_ELIG/INC_TEMP ="1" and /model/instance[1]/data/page-welcome/GRP_ELIG/NO_SEV_ILLNESS ="1" and /model/instance[1]/data/page-welcome/GRP_ELIG/FU_POSSIBLE ="1" and /model/instance[1]/data/page-welcome/GRP_ELIG/SAMPLE_COL_POSSIBLE ="1" and /model/instance[1]/data/page-welcome/GRP_ELIG/PROVIDE_INFORM_CONSENT ="1" and /model/instance[1]/data/page-welcome/GRP_ELIG/FEVER_RESP ="1", "Eligible", if( /model/instance[1]/data/page-welcome/GRP_ELIG/AGE_IC ="0" or /model/instance[1]/data/page-welcome/GRP_ELIG/INC_TEMP ="0" or /model/instance[1]/data/page-welcome/GRP_ELIG/NO_SEV_ILLNESS ="0" or /model/instance[1]/data/page-welcome/GRP_ELIG/FU_POSSIBLE ="0" or /model/instance[1]/data/page-welcome/GRP_ELIG/SAMPLE_COL_POSSIBLE ="0" or /model/instance[1]/data/page-welcome/GRP_ELIG/PROVIDE_INFORM_CONSENT ="0" or /model/instance[1]/data/page-welcome/GRP_ELIG/FEVER_RESP ="0", "Not-Eligible", "nothing"))', 'nothing'); 
+    });
+
+    it(`sequence of "and" clauses and nested if() with sequence of "or" clauses (1)`, () => {
+      assertStringValue(doc, null, 'if( /data/a ="1" and /data/b ="1", "Eligible", if( /data/a ="0" or /data/b ="0", "Not-Eligible", "nothing"))', 'nothing'); 
+    });
+
+    it(`sequence of "and" clauses and nested if() with sequence of "or" clauses (2)`, () => {
+      assertStringValue(doc, null, 'if( /data/a ="1" and /data/c ="1", "Eligible", if( /data/a ="0" or /data/b ="0", "Not-Eligible", "nothing"))', 'Eligible'); 
+    });
+
+    it(`sequence of "and" clauses and nested if() with sequence of "or" clauses (3)`, () => {
+      assertStringValue(doc, null, 'if( /data/c ="1" and /data/b ="1", "Eligible", if( /data/a ="0" or /data/b ="0", "Not-Eligible", "nothing"))', 'ELigible'); 
+    });
+
+    it(`sequence of "and" clauses and nested if() with sequence of "or" clauses (4)`, () => {
+      assertStringValue(doc, null, 'if( /data/a ="1" and /data/b ="1", "Eligible", if( /data/a ="0" or /data/d ="0", "Not-Eligible", "nothing"))', 'Not-Eligible'); 
+    });
+
+    it(`sequence of "and" clauses and nested if() with sequence of "or" clauses (4)`, () => {
+      assertStringValue(doc, null, 'if( /data/a ="1" and /data/b ="1", "Eligible", if( /data/d ="0" or /data/b ="0", "Not-Eligible", "nothing"))', 'Not-Eligible'); 
+    });
 
   });
 });
